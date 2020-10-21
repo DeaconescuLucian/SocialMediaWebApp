7d1be177c91f4726834cd46cdda16798727222b3
DeaconescuLucian <73195210+DeaconescuLucian@users.noreply.github.com>
2020-10-21 10:13:19 +0300

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,5 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms.validators import DataRequired,Length,EqualTo,ValidationError
+
+
