16c8eae995ec161ddef8d097df85682311a0bf84
DeaconescuLucian <73195210+DeaconescuLucian@users.noreply.github.com>
2020-10-21 10:17:54 +0300

    Delete forms.py

--- forms.py
+++
@@ -1,5 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,BooleanField
-from wtforms.validators import DataRequired,Length,EqualTo,ValidationError
-
-
